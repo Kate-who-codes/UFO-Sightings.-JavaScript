a5bf63dcc523721875a4405ada1bf99bf4ce7dda
Kate <68763904+Kate-who-codes@users.noreply.github.com>
2021-04-03 15:35:09 -0700

    Fixed app1-file

--- UFO-level-1/static/js/app1.js
+++ UFO-level-1/static/js/app1.js
@@ -1,7 +1,9 @@
 // Starter Code
+console.log("fff")
+
 var tableData = data;
 var tbody  = d3.select("tbody");
-
+// console.log(tableData);
 // Display the entire dataset as default
 tableData.forEach((report) => {
     console.log(report);
@@ -9,31 +11,32 @@ tableData.forEach((report) => {
 
     Object.defineProperties(report).forEach(([key, value])=> {
         console.log(key, value);
-        var cell = row.append('td');
+        var cell = row.append('td').text(report);
         cell.text(value);
     });
 });
 
 // Select the submit button
-var submit = d3.slect("filter-btn");
+var submit = d3.select("#filter-btn");
 
 //Click event of datetime filter
-submit.on("click", function(){
+button.on("click", function(event){
      d3.select("tbody").html("");
      d3.event.preventDefault();
 //Get the value property of the input element
 var dateTime = d3.select("#datetime").property("value");
-console.log(filterData);
+console.log(filteredData);
 
 // Display the filtereddataset
-filteredDtaset.forEach((report) => {
+filteredDataset.forEach((report) => {
     var row = tbody.append('tr');
 
     Object.defineProperties(report).forEach(([key, value]) => {
         console.log(key, value);
-        var cell = row.append('td');
+        var cell = row.append('td').text(report);
         cell.text(value);
     });
 });
+
 
 });
